bffb1609b4550f4b11e6f1d7c9d3d5418abe5faa
Anthony RICHARD <Anthony.RICHARD@etu.uca.fr>
2023-03-22 19:33:04 +0100

    tests

--- Sources/Stim/Stim/MainPage.xaml.cs
+++ Sources/Stim/Stim/MainPage.xaml.cs
@@ -8,22 +8,5 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-		{
-			if (count == 69)
-				CounterBtn.Text = $"Clicked {count} times, funny number";
-			else
-                CounterBtn.Text = $"Clicked {count} times";
-        }
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
