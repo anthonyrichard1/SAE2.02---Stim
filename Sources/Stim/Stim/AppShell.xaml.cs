48b0922b675c82250adc43dbdad99e7a4c839e0d
Jade VAN BRABANDT <axvanbraba>
2023-03-22 17:16:50 +0100

    UwU

---
+++ Sources/Stim/Stim/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Stim;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Sources/Stim/Stim/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace Stim;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+		{
+			if (count == 69)
+				CounterBtn.Text = $"Clicked {count} times, funny number";
+			else
+                CounterBtn.Text = $"Clicked {count} times";
+        }
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
