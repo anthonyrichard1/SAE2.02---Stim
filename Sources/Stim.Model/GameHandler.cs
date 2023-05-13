48575315e8659f81688c2c06ff772014af1b5a1b
Anthony RICHARD <Anthony.RICHARD@etu.uca.fr>
2023-05-13 09:53:58 +0200

    Déplacement des fonctions Admin dans GameHandler

--- Sources/Stim.Model/GameHandler.cs
+++ Sources/Stim.Model/GameHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,23 @@ namespace Model
         }
         public GameHandler()
         {
-            List<Game> gamesList=new List<Game>();
+            List<Game> gamesList = new List<Game>();
+        }
+        public void AddGametoGamesList(List<Game> gamesList, Game game)
+        {
+            gamesList.Add(game);
+        }
+        public void RemoveGameFromGamesList(List<Game> gamesList, Game game)
+        {
+            gamesList.Remove(game);
+        }
+        public void DelCom(Game game, Review review, int role)
+        {
+            if (role >= 1)
+            {
+                game.RemoveReview(review);
+            }
+            return;
         }
     }
 }
