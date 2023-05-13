732a64ac88de2723b300c4eca5cf1d671015ed91
Anthony RICHARD <Anthony.RICHARD@etu.uca.fr>
2023-05-13 09:55:15 +0200

    Tests unitaires de la classe User

--- Sources/TestProject1/TestUser.cs
+++ Sources/TestProject1/TestUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace Test
 {
-    internal class TestUser
+    public class TestUser
     {
+        [Fact]
+        public void Constructor()
+        {
+            User user = new("username", "biographie", "adresse.mail@gmail.com", "Azerty123*");
+            Assert.NotNull(user);
+        }
+
+        [Fact]
+        public void Username()
+        {
+            User user = new("", "biographie", "adresse.mail@gmail.com", "Azerty123*");
+            Assert.Null(user.Username);
+
+            User user2 = new(null, "biographie", "adresse.mail@gmail.com", "Azerty123*");
+            Assert.Null(user.Username);
+        }
     }
 }
