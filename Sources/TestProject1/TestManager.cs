4d6ad225d163b897846b93a979e9e09b2cd150f5
Anthony RICHARD <Anthony.RICHARD@etu.uca.fr>
2023-05-22 20:28:44 +0100

    Comment testManager

--- Sources/TestProject1/TestManager.cs
+++ Sources/TestProject1/TestManager.cs
@@ -5,25 +5,25 @@ namespace Test
 {
     public class TestManager
     {
-        [Fact]
-        public void Constructor()
-        {
-            IPersistance persistance = new Persistance();
-            Manager manager = new(persistance);
-            Assert.NotNull(manager);
-        }
+        //[Fact]
+        //public void Constructor()
+        //{
+        //    IPersistance persistance = new Persistance();
+        //    Manager manager = new(persistance);
+        //    Assert.NotNull(manager);
+        //}
 
-        [Fact]
-        public void AddAndRemoveGame()
-        {
-            IPersistance persistance = new Persistance();
-            Manager manager = new(persistance);
-            Game game = new("game", "description", 2012, new List<String> { "1", "2", "3" }, "cover");
-            manager.AddGametoGamesList(game);
+        //[Fact]
+        //public void AddAndRemoveGame()
+        //{
+        //    IPersistance persistance = new Persistance();
+        //    Manager manager = new(persistance);
+        //    Game game = new("game", "description", 2012, new List<String> { "1", "2", "3" }, "cover");
+        //    manager.AddGametoGamesList(game);
 
-            Assert.Contains(game, manager.GameList);
-            manager.RemoveGameFromGamesList(game);
-            Assert.DoesNotContain(game, manager.GameList);
-        }
+        //    Assert.Contains(game, manager.GameList);
+        //    manager.RemoveGameFromGamesList(game);
+        //    Assert.DoesNotContain(game, manager.GameList);
+        //}
     }
 }
