336ccda5bb70c932aa240d22784b0810d11922f6
Jade_VAN_BRABANDT <Jade.VAN_BRABANDT@etu.uca.fr>
2023-05-24 15:43:13 +0200

    Feat : sauvegarde à la fermeture, création de fichier si non existant

--- Sources/Stim/App.xaml.cs
+++ Sources/Stim/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Model;
 using StimPersistance;
+using StimStub;
 
 namespace Stim;
 
@@ -13,7 +14,20 @@ public partial class App : Application
 	{
 		InitializeComponent();
         string mainDir = FileSystem.Current.AppDataDirectory;
-        Mgr = new Manager(new Persistance(mainDir));
+        if (File.Exists(Path.Combine(mainDir,"games.xml"))) Mgr = new Manager(new Persistance(mainDir));
+        else Mgr = new Manager(new Stub());
 		MainPage = new AppShell();
 	}
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        window.Stopped += (s, e) =>
+        {
+            Mgr._persistance = new Persistance(FileSystem.Current.AppDataDirectory);
+            Mgr.SaveGames();
+        };
+
+        return window;
+    }
 }
