3ade9abc80bfce9dd29457bdc728eaa02c646475
Anthony RICHARD <Anthony.RICHARD@etu.uca.fr>
2023-05-24 17:06:46 +0200

    Ajout d'un jeu à la gameList (il manque juste la cover normalement)

--- Sources/Stim/AddGamePage.xaml.cs
+++ Sources/Stim/AddGamePage.xaml.cs
@@ -5,6 +5,7 @@ namespace Stim;
 public partial class AddGamePage : ContentPage
 {
 	public Manager Manager { get; set; }
+
 	public AddGamePage(Manager Mgr)
 	{
 		InitializeComponent();
@@ -13,10 +14,10 @@ public partial class AddGamePage : ContentPage
 
 	private void AddGame(object sender, EventArgs e)
 	{
-		//faut rajouter des tests
-		string tag1 = TagEntry1.Text;
-        string tag2 = TagEntry2.Text;
-        string tag3 = TagEntry3.Text;
-		Manager.AddGametoGamesList(new Game(NameEntry.Text, DescriptionEntry.Text, int.Parse(YearEntry.Text), new List<string> { tag1, tag2, tag3 }, "null", "www.link.com"));
+		int year;
+		if (string.IsNullOrEmpty(NameEntry.Text) || string.IsNullOrEmpty(DescriptionEntry.Text) || string.IsNullOrEmpty(YearEntry.Text) || !int.TryParse(YearEntry.Text, out year) || string.IsNullOrWhiteSpace(LinkEntry.Text)) return;
+
+		Manager.AddGametoGamesList(new Game(NameEntry.Text, DescriptionEntry.Text, year, new List<string> { TagEntry1.Text, TagEntry2.Text, TagEntry3.Text }, "nyancat.png", LinkEntry.Text));
+		Navigation.RemovePage(this);
 	}
 }
