f177b1521a468c02b720868505252f518400ac46
Jade_VAN_BRABANDT <Jade.VAN_BRABANDT@etu.uca.fr>
2023-05-24 18:40:51 +0200

    fix : persistance !

--- Sources/Stim/App.xaml.cs
+++ Sources/Stim/App.xaml.cs
@@ -11,6 +11,20 @@ public partial class App : Application
     {
         InitializeComponent();
         MainPage = new AppShell();
-        Manager = new(new Stub());
+        if (File.Exists(Path.Combine(FileSystem.Current.AppDataDirectory, "games.xml"))) Manager = new Manager(new Persistance(FileSystem.Current.AppDataDirectory));
+        else Manager = new(new Stub());
     }
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        window.Stopped += (s, e) =>
+        {
+            Manager._persistance = new Persistance(FileSystem.Current.AppDataDirectory);
+            var test = Manager;
+            Manager.SaveGames();
+        };
+
+        return window;
+    } 
 }
