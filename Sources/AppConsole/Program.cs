3576f8a7b324746da05df500e9dcbf2a6016518f
Anthony RICHARD <Anthony.RICHARD@etu.uca.fr>
2023-06-05 14:07:00 +0200

    début tests fonctionnels

--- Sources/AppConsole/Program.cs
+++ Sources/AppConsole/Program.cs
@@ -11,7 +11,22 @@ namespace AppConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Program.Menu();
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("-----MENU PRINCIPAL-----\n" +
+                "1-Ajouter un jeu\n" +
+                "2-Supprimer un jeu\n" +
+                "3-Afficher les jeux\n" +
+                "4-Ajouter un utilisateur\n" +
+                "5-Afficher les utilisateur\n" +
+                "6-Afficher les commentaires d'un jeu\n" +
+                "7-Ajouter un commentaire\n" +
+                "8-Supprimer un commentaire\n" +
+                "9-Ajouter jeu suivis\n" +
+                "10-Supprimer jeu suivis\n");
         }
     }
 }
