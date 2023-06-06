2a40713a1d98a9fc5b33828f8d94818a537db904
Anthony RICHARD <Anthony.RICHARD@etu.uca.fr>
2023-06-06 17:29:43 +0200

    converter pas fini

---
+++ Sources/Stim/DoubleToStar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stim
+{
+    public class DoubleToStar : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter != null && parameter is string && !string.IsNullOrEmpty(parameter as string))
+            {
+                string param = parameter as string;
+                double rate = double.Parse(param.Split('|')[0]);
+                int pos = int.Parse(param.Split('|')[0]);
+
+                if (pos <= rate ) return "etoile_pleine.png";
+                if (pos - 1 < rate && rate < pos) return "etoile_mi_plein.png";
+            }
+            return "etoile_vide.png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
